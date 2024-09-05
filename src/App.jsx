7fc9a7462b9b9dca1660f9294a35cb90c63f0576
Hadi Jamaludin <hadijmldn@gmail.com>
2024-09-05 11:34:55 +0700

    123

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import About from './pages/About'
+import Explore from './pages/Explore'
+
+function App() {
+
+  return (
+    <>
+     <About />
+     <Explore />
+    </>
+  )
+}
+
+export default App
